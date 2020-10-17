50bc34bef81f6cfaa42a62b560554f7617ea5467
Mingoose <44516061+Mingoose@users.noreply.github.com>
2020-10-17 16:35:58 -0400

    Add files via upload

--- wavToPng.py
+++ wavToPng.py
@@ -1,7 +1,9 @@
 import scipy.io.wavfile
 import math
 from PIL import Image
+
 def sigmoid(x):
+   
     if x < -700:
         x = -700
  
@@ -14,22 +16,34 @@ def sigmoid(x):
 
 
 
-out = scipy.io.wavfile.read("audio.wav", mmap=False)
+out = scipy.io.wavfile.read("rickroll.wav", mmap=False)
 a,b = out
 print(out)
 
 list1 = b.tolist()
-print("len")
-print(len(list1))
+list2 = []
+
+if isinstance(list1[0],list):
+    for i in range(len(list1)):
+        total = 0
+        for j in range(len(list1[i])):
+            total += list1[i][j]
+        list2.append(total/len(list1[i]))
+        
+if isinstance(list1[0],list):
+    list1 = list2
+    
+
+
 for i in range(len(list1)):
     list1[i] = int(255*sigmoid(list1[i]))
  
-#print(list1[:100])
+
 img = Image.new('RGB',(300,300))
 pixels = img.load()
 count = 0
+
 for i in range(img.size[0]):
-    print(i)
     for j in range(img.size[1]):
         pixels[i,j] = (list1[count],list1[count+1],list1[count+2])
         count += 3
